4f5a65f48532e137d1ea7e96d37045d864071d0a
Owen Williams <hi@owened.co.nz>
2013-12-30 15:42:10 +1300

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,14 +15,26 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
-  it { should be_valid }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
-  describe "remember token" do
+  it { should be_valid }
+  it { should_not be_admin }
+
+  it { should respond_to(:remember_token) }
+
+describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+end
+
+describe "remember token" do
     before { @user.save }
     its(:remember_token) {should_not be_blank}
-  end
+end
 describe "when password is not present" do
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
